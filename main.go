bc38f9a49e73692e6ce9b9ffaa286ad13eaa3e25
nabbat <dubrov@me.com>
2023-10-09 21:54:44 +0200

    iter3 completed

--- main.go
+++ main.go
@@ -3,21 +3,28 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
+	"github.com/gorilla/mux"
 	"io"
 	"log"
 	"net/http"
 )
 
 // Словарь для хранения соответствий между сокращёнными и оригинальными URL
-var urlMap map[string]string
+// TODO Создать хранилище
+var urlMap = map[string]string{}
 
 // Перенаправляем по полной ссылке
 func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "invalid request type", http.StatusBadRequest)
+		return
+	}
+	// Добавляем тестовое соответствие в словарь
+	urlMap["aHR0cH"] = "https://practicum.yandex.ru/"
 	// Получаем идентификатор из URL-пути
 	id := r.URL.Path[1:]
 
 	// Получаем оригинальный URL из словаря
-	// TODO Создать хранилище
 
 	if originalURL, found := urlMap[id]; found {
 		// Устанавливаем заголовок Location и возвращаем ответ с кодом 307
@@ -36,7 +43,14 @@ func shortenURLHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Ошибка чтения запроса", http.StatusBadRequest)
 		return
 	}
+	/*
+		contentType := r.Header.Get("Content-Type")
 
+		if contentType != "text/plain" || contentType != "text/plain; charset=utf-8" {
+			http.Error(w, "invalid request type", http.StatusBadRequest)
+			return
+		}
+	*/
 	// Преобразуем в строку
 	url := string(urlBytes)
 
@@ -66,19 +80,11 @@ func generateID(fullURL string) string {
 }
 
 func main() {
-	mux := http.NewServeMux()
-	urlMap = make(map[string]string)
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// GET OR POST
-		if r.Method != http.MethodPost {
-			redirectHandler(w, r)
-			return
-		}
-		shortenURLHandler(w, r)
+	r := mux.NewRouter()
+	r.HandleFunc("/{idShortenURL}", redirectHandler).Methods("GET")
+	r.HandleFunc("/", shortenURLHandler).Methods("POST")
 
-	})
-
-	err := http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		panic(err)
 	}
--- main_test.go
+++ main_test.go
@@ -1,62 +1,124 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
 	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
 func Test_generateID(t *testing.T) {
-	type args struct {
-		fullURL string
-	}
 	tests := []struct {
-		name string
-		args args
-		want string
+		name    string
+		fullURL string
+		want    string
 	}{
-		// TODO: Add test cases.
+		{name: "Проверим генератор на пустую строку", fullURL: "", want: ""},
+		{name: "Проверим генератор на НЕпустую строку", fullURL: "https://practicum.yandex.ru/", want: "aHR0cH"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := generateID(tt.args.fullURL); got != tt.want {
+			if got := generateID(tt.fullURL); got != tt.want {
 				t.Errorf("generateID() = %v, want %v", got, tt.want)
 			}
 		})
 	}
 }
 
-func Test_redirectHandler(t *testing.T) {
-	type args struct {
-		w http.ResponseWriter
-		r *http.Request
+func Test_shortenURLHandler(t *testing.T) {
+	type want struct {
+		code        int
+		response    string
+		contentType string
 	}
+
 	tests := []struct {
-		name string
-		args args
+		name                string
+		bodyURL             string
+		responseContentType string
+		targetURL           string
+		want                want
 	}{
-		// TODO: Add test cases.
+		{name: "positive POST shortenURLHandler",
+			bodyURL:             "https://practicum.yandex.ru/",
+			responseContentType: "text/plain",
+			targetURL:           "/",
+			want: want{
+				code:        201,
+				response:    `http://localhost:8080/aHR0cH`,
+				contentType: "text/plain",
+			},
+		},
+		/*
+			{name: "negative POST shortenURLHandler",
+				bodyURL:             "https://practicum.yandex.ru/",
+				responseContentType: "application/json",
+				targetURL:           "/",
+				want: want{
+					code:        201,
+					response:    "http://localhost:8080/aHR0cH",
+					contentType: "text/plain; charset=utf-8",
+				},
+			},*/
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			redirectHandler(tt.args.w, tt.args.r)
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			bodyURLTesting := strings.NewReader(test.bodyURL)
+			request := httptest.NewRequest(http.MethodPost, test.targetURL, bodyURLTesting)
+			request.Header.Add("Content-Type", test.responseContentType)
+			// создаём новый Recorder
+			w := httptest.NewRecorder()
+			shortenURLHandler(w, request)
+			res := w.Result()
+			// проверяем код ответа
+			assert.Equal(t, res.StatusCode, test.want.code)
+			// получаем и проверяем тело запроса
+			defer res.Body.Close()
+			resBody, err := io.ReadAll(res.Body)
+
+			require.NoError(t, err)
+			assert.Equal(t, string(resBody), test.want.response)
+			assert.Equal(t, res.Header.Get("Content-Type"), test.want.contentType)
 		})
 	}
 }
 
-func Test_shortenURLHandler(t *testing.T) {
-	type args struct {
-		w http.ResponseWriter
-		r *http.Request
+func Test_redirectHandler(t *testing.T) {
+	type want struct {
+		code     int
+		response string
 	}
 	tests := []struct {
 		name string
-		args args
+		want want
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Проверяем redirect",
+			want: want{
+				code:     307,
+				response: `https://practicum.yandex.ru/`,
+			},
+		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			shortenURLHandler(tt.args.w, tt.args.r)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodGet, "/aHR0cH", nil)
+			// создаём новый Recorder
+			w := httptest.NewRecorder()
+			redirectHandler(w, request)
+
+			res := w.Result()
+			// проверяем код ответа
+			assert.Equal(t, res.StatusCode, test.want.code)
+			// получаем и проверяем тело запроса
+			defer res.Body.Close()
+			resBody, err := io.ReadAll(res.Body)
+
+			require.NoError(t, err, string(resBody))
 		})
 	}
 }
